b307c85ee5bdae50fea9d63df3f26d8c07abd7d3
Rafael de Freitas <167362524+devfreitas@users.noreply.github.com>
2026-03-24 18:16:58 -0300

    Add files via upload

---
+++ ModificadoresDeAcesso/ModificadoresDeAcesso/Core/ServicoPagamento.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// NAMESPACE: Organização lógica do código
+namespace ModificadoresDeAcesso.Core
+{
+    // INTERFACE: O contrato que define a ação de pagar
+    public interface IPagamento
+    {
+        void ProcessarPagamento(decimal valor);
+    }
+
+    // CLASSE BASE: Usando modificadores de acesso
+    public class ServicoPagamento
+    {
+        // CAMEL CASE
+        private string _chaveTransacao {  get; set; }
+
+        // CONSTRUTOR: Inicializa o estado do objeto
+        public ServicoPagamento(string _chaveTransacao)
+        {
+            // 'this' diferencia o campo do parâmetro
+            this._chaveTransacao = _chaveTransacao;
+        }
+
+        // Modificador 'protected': visível apenas para esta classe e herdeiros
+        protected void LogarTransacao(string mensagem)
+        {
+            Console.WriteLine($"[LOG - {DateTime.Now}: {mensagem}]");
+        }
+    }
+
+    // PALAVRA-CHAVE 'class' e Herança/Implementação
+    public class PagamentoCartao : ServicoPagamento, IPagamento
+    {
+        public decimal LimiteDepagamento { get; set; } = 300.00m;
+
+        // Construtor chamando a base
+        public PagamentoCartao(string _chaveTransacao) : base(_chaveTransacao)
+        {
+
+        }
+
+        //Camel Case no parâmetro 'valor'
+        public void ProcessarPagamento(decimal valor)
+        {
+            LogarTransacao($"Processando o pagamando R${valor} via Cartão");
+
+            if (valor > LimiteDepagamento)
+            {
+                Console.WriteLine("Pagamento não aprovado!");
+            }
+            else 
+            {
+                Console.WriteLine("Pagamento aprovado com sucesso!");
+            }
+        }
+    }
+
+    public class PagamentoBoleto : ServicoPagamento, IPagamento
+    {
+        // Construtor chamando a base
+        public PagamentoBoleto(string _chaveTransacao) : base(_chaveTransacao)
+        {
+
+        }
+
+        //Camel Case no parâmetro 'valor'
+        public void ProcessarPagamento(decimal valor)
+        {
+            LogarTransacao($"Processando o pagamando R${valor} via Boleto");
+            Console.WriteLine("Pagamento aprovado com sucesso!");
+        }
+    }
+}
---
+++ ModificadoresDeAcesso/ModificadoresDeAcesso/Program.cs
@@ -0,0 +1,27 @@
+﻿using ModificadoresDeAcesso.Core;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ModificadoresDeAcesso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Sistema de Pagamento ====");
+
+            //Pagamento via Boleto
+            IPagamento pagamentoBoleto = new PagamentoBoleto("API-KEY-1234");
+            pagamentoBoleto.ProcessarPagamento(150.00m);
+
+            //Pagamento via Cartao
+            IPagamento pagamentoCartao = new PagamentoCartao("API-KEY-1234");
+            decimal valorDaCompra = 301.00m;
+            pagamentoCartao.ProcessarPagamento(valorDaCompra);
+
+            Console.WriteLine("==== Sistema de Pagamento ====");
+            Console.ReadKey();
+
+        }
+    }
+}
